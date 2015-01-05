db4e44d8b0c11f56f8501cc7525d82c39b1b518a
iCHAIT <cgupta319@gmail.com>
2015-01-05 21:58:19 +0530

    Incorrect input scenario handled

--- agecalulator/agecalculator2.py
+++ agecalulator/agecalculator2.py
@@ -1,14 +1,19 @@
 import sys
 from datetime import date
 
-yr = int(raw_input('Enter the year you were born(yyyy):'))
-mon = int(raw_input('Enter the month you were born(mm):'))
-dat = int(raw_input('Enter the date you were born(dd):'))
+try:
+	yr = int(raw_input('Enter the year you were born(yyyy):'))
+	mon = int(raw_input('Enter the month you were born(mm):'))
+	dat = int(raw_input('Enter the date you were born(dd):'))
 
-birthday = date(yr,mon,dat)
+	birthday = date(yr,mon,dat)
 
-now = date.today()
-age = now - birthday
+	now = date.today()
+	age = now - birthday
 
-print "You are %d years old " %(age.days/365)
-print "You've lived %d days" %(age.days)
+	print "You are %d years old " %(age.days/365)
+	print "You've lived %d days" %(age.days)
+
+except ValueError, err:
+        print 'ERROR:', err	
+	
