0cb790afcbbbc1b434b7c26d3713cc3988495af3
iCHAIT <cgupta319@gmail.com>
2015-01-07 22:00:52 +0530

    Minor change

--- todo_manager.py
+++ todo_manager.py
@@ -1,11 +1,13 @@
 import random 
 
-with open('TODO.txt', "r+") as f:
+with open('TODO.txt', "r+") as f:  #I've kept my TODO list in TODO.txt
 	lines = f.readlines()
 	task = str(random.choice(lines))	
 	print task
+    
 	f.seek(0)
 	f.truncate()
+    
 	for line in lines:
   		if line != task:
    			f.write(line)
