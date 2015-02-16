79d05baf498e710c6ed2d44a511c3cf04d77c1be
iCHAIT <cgupta319@gmail.com>
2015-02-16 14:21:11 +0530

    sot_dwnlds.py added

---
+++ sort_dwnlds.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import getpass
+import shutil
+
+user = getpass.getuser()
+
+print user
+
+dir="/Users/"+user+"/Downloads/"
+
+def sort(f_type):
+  try:  
+      dirname=f_type.upper() 
+      os.chdir(dir)
+      for files in os.listdir("."):
+  
+	  if files.endswith(f_type):
+	
+	      if os.path.isdir(dir+dirname):
+		  shutil.move(dir+files,dir+dirname)
+        
+	      else:
+		  os.mkdir(dir+dirname)
+		  shutil.move(dir+files,dir+dirname)
+		
+      return	  
+  
+  except IOError:
+      print "Error in reading file"
+
+sort("scpt")
+sort("dmg")
+sort("mp3")
+sort("rpm")
+sort("zip")
+sort("pdf")
+sort("doc")
+sort("gz")
+sort("jpg")
+sort("flv")
+sort("tar")
+sort("torrent")
+sort("txt")
+sort("zip")
+sort("exe")
+sort("bz")
+sort("dotx")
+sort("csv")
+sort("iso")
+sort("download")
+sort("pkg")
+sort("html")
+sort("ics")
+sort("whl")
+sort("rar")
+sort("epub")
