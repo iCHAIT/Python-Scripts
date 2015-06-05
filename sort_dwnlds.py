c45581abf767df845c2fcb0fdaa39879c8e580cd
iCHAIT <cgupta319@gmail.com>
2015-06-06 00:57:28 +0530

    rewriting the code

--- sort_dwnlds.py
+++ sort_dwnlds.py
@@ -2,55 +2,22 @@ import os
 import shutil
 
 user = os.getenv("USER")
-dir = "/Users/"+user+"/Downloads/"
+dir = "/Users/"+user+"/Desktop/test/"
+# dir = "/Users/"+user+"/Downloads/"
 
+try:
+    os.chdir(dir)
+    for files in os.listdir("."):
+        print files
+        extn = files.split(".")[0]
+        print extn
+        dirname = extn.upper()
+        if os.path.isdir(dir+dirname):
+            shutil.move(dir+files, dir+dirname)
 
-def sort(f_type):
-    try:
-        dirname = f_type.upper()
-        os.chdir(dir)
-        for files in os.listdir("."):
+        else:
+            os.mkdir(dir+dirname)
+            shutil.move(dir+files, dir+dirname)
 
-            if files.endswith(f_type):
-
-                if os.path.isdir(dir+dirname):
-                    shutil.move(dir+files, dir+dirname)
-
-                else:
-                    os.mkdir(dir+dirname)
-                    shutil.move(dir+files, dir+dirname)
-
-        return
-
-    except IOError:
-        print "Error in reading file"
-
-sort("scpt")
-sort("dmg")
-sort("mp3")
-sort("rpm")
-sort("zip")
-sort("pdf")
-sort("doc")
-sort("gz")
-sort("jpg")
-sort("flv")
-sort("tar")
-sort("torrent")
-sort("txt")
-sort("zip")
-sort("exe")
-sort("bz")
-sort("dotx")
-sort("csv")
-sort("iso")
-sort("download")
-sort("pkg")
-sort("html")
-sort("ics")
-sort("whl")
-sort("rar")
-sort("epub")
-sort("png")
-sort("odp")
-sort("ppt")
+except IOError:
+    print "Error in reading file"
