7b4d7192786eb82799fd0ae3d6b31e0271dff246
iCHAIT <cgupta319@gmail.com>
2015-06-07 18:09:10 +0530

    os.path.join used rather than adding paths.

--- The Sorting Hat/sorting-hat.py
+++ The Sorting Hat/sorting-hat.py
@@ -2,26 +2,30 @@ import os
 import shutil
 
 user = os.getenv("USER")
-direc = "/Users/"+user+"/Downloads/"
-# direc = "/Users/"+user+"/Desktop/hat/"
+# direc = "/Users/"+user+"/Downloads/"
+direc = "/Users/"+user+"/Desktop/hat/"
 
 
-def findExtension():
+def findExtension(files):
+    # Find the extension of the file
     extn = os.path.splitext(files)[1].replace('.', '')
     dirname = extn.upper()
     return dirname
 
 
-def ifFileExist():
-    if os.path.isfile(direc+dirname+"/"+files):
-        print "Could not move "+direc+files+" it already exist in "+direc+dirname+"/"
+def FileExist(direc, dirname, files):
+    # Check if the file to be sorted already exist in the target subdirectory.
+    # if it doesn't then move it.
+    if os.path.isfile(os.path.join(direc, dirname, files)):
+        print "Could not move "+os.path.join(direc, files)+" it already exist in "+os.path.join(direc, dirname)
         return
-    shutil.move(direc+files, direc+dirname)
+    shutil.move(os.path.join(direc, files), os.path.join(direc, dirname))
 
 
-def makethedir():
-    os.mkdir(direc+dirname)
-    shutil.move(direc+files, direc+dirname)
+def makeTheDir(files):
+    # Make the target sub directory.
+    os.mkdir(os.path.join(direc, dirname))
+    shutil.move(os.path.join(direc, files), os.path.join(direc, dirname))
 
 
 try:
@@ -29,12 +33,12 @@ try:
     for files in os.listdir(direc):
         if not files.startswith('.'):    # Ignoring hidden files
             if os.path.isfile(files):
-                dirname = findExtension();
-                if os.path.isdir(direc+dirname):
-                    ifFileExist();
+                dirname = findExtension(files);
+                if os.path.isdir(os.path.join(direc, dirname)):
+                    FileExist(direc, dirname, files);
 
                 else:
-                    makethedir();
+                    makeTheDir(files);
     print "All Done!"
 
 except IOError:
