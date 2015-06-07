7b996d143118ecaee0fd3df9b4f63f5501ecb9d3
iCHAIT <cgupta319@gmail.com>
2015-06-07 17:16:47 +0530

    code modified

--- The Sorting Hat/sorting-hat.py
+++ The Sorting Hat/sorting-hat.py
@@ -2,24 +2,39 @@ import os
 import shutil
 
 user = os.getenv("USER")
-dir = "/Users/"+user+"/Downloads/"
+# dir = "/Users/"+user+"/Downloads/"
+direc = "/Users/"+user+"/Desktop/hat/"
+
+
+def findExtension():
+    extn = os.path.splitext(files)[1].replace('.', '')
+    dirname = extn.upper()
+    return dirname
+
+
+def ifFileExist():
+    if os.path.isfile(direc+dirname+"/"+files):
+        print "Could not move "+direc+files+" it already exist in "+direc+dirname+"/"
+        return
+    shutil.move(direc+files, direc+dirname)
+
+
+def makethedir():
+    os.mkdir(direc+dirname)
+    shutil.move(direc+files, direc+dirname)
+
 
 try:
-    os.chdir(dir)
-    for files in os.listdir(dir):
+    os.chdir(direc)
+    for files in os.listdir(direc):
         if not files.startswith('.'):    # Ignoring hidden files
             if os.path.isfile(files):
-                extn = os.path.splitext(files)[1].replace('.', '')
-                dirname = extn.upper()
-                if os.path.isdir(dir+dirname):
-                    if os.path.isfile(dir+dirname+"/"+files):
-                        print "Could not move "+dir+files+" it already exist in "+dir+dirname+"/"
-                        continue
-                    shutil.move(dir+files, dir+dirname)
+                dirname = findExtension();
+                if os.path.isdir(direc+dirname):
+                    ifFileExist();
 
                 else:
-                    os.mkdir(dir+dirname)
-                    shutil.move(dir+files, dir+dirname)
+                    makethedir();
     print "All Done!"
 
 except IOError:
